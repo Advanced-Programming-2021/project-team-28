39530f9d48a48713e135d660c7f06fbd4692f673
mohammadmahdihn <mohammadmahdiheydarinasab@gmail.com>
2021-04-23 18:43:23 +0430

    test git in intellij again

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Test GIT In Intellij");
+        System.out.println("Test GIT In Intellij again");
     }
 }
