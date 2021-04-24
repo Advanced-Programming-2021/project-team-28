fcd1febb0cea44af99e11196a3b413bd7a6bd4a6
Haaji <soldier.mh.2001@gmail.com>
2021-04-24 07:28:27 +0430

    some test

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println(" I have to be p01it3");
     }
 }
--- src/main/java/model/Deck.java
+++ src/main/java/model/Deck.java
@@ -1,4 +1,137 @@
 package model;
 
 public class Deck {
+
+//    private User user;
+//    private String deckName;
+//    private int numofMainDeckCards;
+//    private int numOfSideDeckCards;
+//    private ArrayList <Card> allCardsInMainDeck;
+//    private ArrayList <Card> allCardsInSideDeck;
+//
+//
+//    public <<constructor>> Deck(String deckName, User deckCreator)
+//    {
+//
+//    }
+//
+//    public void addDeckToUserDecks(User user)
+//    {
+//
+//    }
+//
+//    public void sortDeck()
+//    {
+//
+//    }
+//
+//    public Deck getDeckByDeckName(String deckName, User user)
+//    {
+//
+//    }
+//
+//    public boolean doesUserHaveThisDeck(User user, String deckName)
+//    {
+//
+//    }
+//
+//    public void createDeck(User user, String deckName)
+//    {
+//
+//    }
+//
+//    public String decksArrayListToString(ArrayList <Deck> decks)
+//    {
+//
+//    }
+//
+//    public void removeDeck(User user)
+//    {
+//
+//    }
+//
+//    public void activateDeck(User user)
+//    {
+//
+//    }
+//
+//    public void addCardToDeck(String cardName, User user, boolean isForSideDeck)
+//    {
+//
+//    }
+//
+//    public void removeCardFromDeck(String cardName, User user, boolean isFromSideDeck)
+//    {
+//
+//    }
+//
+//    public String showDeck(String deckName, boolean isSideDeck)
+//    {
+//
+//    }
+//
+//    public boolean isMainDeckFull()
+//    {
+//
+//    }
+//
+//    public boolean isSideDeckFull()
+//    {
+//
+//    }
+//
+//    public boolean doesDeckHaveThreeCardsFromThisTypeOfCard(String cardName)
+//    {
+//
+//    }
+//
+//    public String toString()
+//    {
+//
+//    }
+//
+//    public String getDeckName()
+//    {
+//
+//    }
+//
+//    public void setDeckName(String deckName)
+//    {
+//
+//    }
+//
+//    public int getNumOfMainCards()
+//    {
+//
+//    }
+//
+//    public void increaseNumOfMainCards(int numOfAddedCards)
+//    {
+//
+//    }
+//
+//    public void decreaseNumOfMainCards(int numOfRemovedCards)
+//    {
+//
+//    }
+//
+//    public int getNumOfSideCards()
+//    {
+//
+//    }
+//
+//    public void increaseNumOfSideCards(int numOfAddedCards)
+//    {
+//
+//    }
+//
+//    public void decreaseNumOfSideCards(int numOfRemovedCards)
+//    {
+//
+//    }
+//
+//    public boolean isDeckValid()
+//    {
+//
+//    }
 }
