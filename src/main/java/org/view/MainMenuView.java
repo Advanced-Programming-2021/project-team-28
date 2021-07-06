ebadc687cfe18d560eb2163b077c431e049c70d4
alirf2077 <alirf2077@gmail.com>
2021-07-06 16:24:21 +0430

    main menu half design

--- src/main/java/org/view/MainMenuView.java
+++ src/main/java/org/view/MainMenuView.java
@@ -1,8 +1,13 @@
 package org.view;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.effect.Lighting;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import org.controller.DeckMenuController;
@@ -11,6 +16,23 @@ import org.controller.MainMenuController;
 import java.util.Scanner;
 
 public class MainMenuView extends Application {
+
+    @FXML
+    private Button start;
+    @FXML
+    private Button shop;
+    @FXML
+    private Button deck;
+    @FXML
+    private Button profile;
+    @FXML
+    private Button logout;
+    @FXML
+    private Button score;
+    @FXML
+    private Button export;
+
+
     private static Stage primaryStage;
     MainMenuController controller;
     public Text userText;
@@ -47,9 +69,111 @@ public class MainMenuView extends Application {
         loader.setLocation(getClass().getResource("/mainclass/mainmenu.fxml"));
         Scene scene = new Scene(loader.load(), 1280, 720);
         stage.setTitle("Main Menu");
+        buttonSet();
         setUserText();
         stage.setScene(scene);
         stage.show();
+    }
+
+    private void buttonSet() {
+        logout.setOnMouseEntered(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                logout.setEffect(new Lighting());
+            }
+        });
+
+        logout.setOnMouseExited(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                logout.setEffect(null);
+            }
+        });
+
+        export.setOnMouseEntered(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                export.setEffect(new Lighting());
+            }
+        });
+
+        export.setOnMouseExited(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                export.setEffect(null);
+            }
+        });
+
+        start.setOnMouseEntered(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                start.setEffect(new Lighting());
+            }
+        });
+
+        start.setOnMouseExited(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                start.setEffect(null);
+            }
+        });
+
+        score.setOnMouseEntered(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                score.setEffect(new Lighting());
+            }
+        });
+
+        score.setOnMouseExited(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                score.setEffect(null);
+            }
+        });
+
+        profile.setOnMouseEntered(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                profile.setEffect(new Lighting());
+            }
+        });
+
+        profile.setOnMouseExited(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                profile.setEffect(null);
+            }
+        });
+
+        deck.setOnMouseEntered(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                deck.setEffect(new Lighting());
+            }
+        });
+
+        deck.setOnMouseExited(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                deck.setEffect(null);
+            }
+        });
+
+        shop.setOnMouseEntered(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                shop.setEffect(new Lighting());
+            }
+        });
+
+        shop.setOnMouseExited(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                shop.setEffect(null);
+            }
+        });
+
     }
 
     public void logout() {
