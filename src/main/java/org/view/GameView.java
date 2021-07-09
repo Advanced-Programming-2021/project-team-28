7ecba8c1a2b07af8cf39b7bf39678f90366c1502
mohammadmahdihn <mohammadmahdiheydarinasab@gmail.com>
2021-07-09 20:04:57 +0430

    Still Game

--- src/main/java/org/view/GameView.java
+++ src/main/java/org/view/GameView.java
@@ -5,7 +5,6 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.ScrollPane;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
